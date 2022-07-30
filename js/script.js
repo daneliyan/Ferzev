b8f81bf0fbab2cf2bb81e1e40e4c829422f46ac9
daneliyan <105802998+daneliyan@users.noreply.github.com>
2022-07-30 15:59:37 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,12 +7,14 @@ var swiper = new Swiper(".mySwiper", {
 
 
 const content = document.querySelector('.slider__video-content');
-const play = document.querySelector('.btn__play');
-const video = document.querySelector('.slider__video video');
+const play = document.querySelectorAll('.btn__play');
+const video = document.querySelector('video');
 
-play.addEventListener('click', () => {
-    video.play();
-    video.setAttribute('controls', 'controls');
-    play.classList.add('btn__play--hidden');
-    content.classList.add('slider__video-content--hidden')
+play.forEach((btn__play) => {
+    btn__play.addEventListener('click', () => {
+        video.play();
+        video.setAttribute('controls', 'controls');
+        btn__play.classList.add('btn__play--hidden');
+        content.classList.add('slider__video-content--hidden')
+    })
 });
